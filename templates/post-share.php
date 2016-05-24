90c8ae1731b5e950df36660f76acf096d103aa6a
Daniel Aguilar <dacmail@gmail.com>
2016-05-24 13:23:48 +0200

    whatsapp y telegram

--- templates/post-share.php
+++ templates/post-share.php
@@ -1,5 +1,6 @@
 <div class="post-share">
 	<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" class="icon fb" target="_blank"><i class="fa fa-facebook"></i></a>
 	<a href="https://twitter.com/intent/tweet?url=<?php the_permalink(); ?>&text=<?php the_title_attribute(); ?>&via=porCausaorg" class="icon tw" target="_blank"><i class="fa fa-twitter"></i></a>
-	<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php the_title_attribute(); ?>&summary=<?php echo wp_kses(get_the_excerpt(), array()); ?>" class="icon in" target="_blank"><i class="fa fa-linkedin"></i></a>
+	<a href="whatsapp://send?text=<?php the_title(); ?> – <?php urlencode(the_permalink()); ?>" data-action="share/whatsapp/share" class="icon wa" target="_blank"><i class="fa fa-whatsapp"></i></a>
+	<a href="https://telegram.me/share/url?url=<?php urlencode(the_permalink()); ?>" class="icon tg" target="_blank"><i class="fa fa-paper-plane"></i></a>
 </div>
