184d93fb5bc5113df9472cd530cfcaedc44e29fb
Daniel Aguilar <dacmail@gmail.com>
2016-05-27 10:04:25 +0200

    Página 404

--- 404.php
+++ 404.php
@@ -7,6 +7,13 @@
 			</h1>
 
 			<div class="post-content clearfix">
+				<p>Los sentimos, la página que buscas no existe, puede que la URL esté mal escrita o que el recurso haya dejado de existir, puedes probar haciendo una búsqueda o volver a la página de inicio.</p>
+				<form action="<?php home_url('/'); ?>">
+					<p>
+						<label for="s" class="obligatorio"><input id="s" name="s" placeholder="¿Qué estás buscando?" type="text"></label>
+						<button type="submit" class="btn btn-next btn-primary">Buscar</button>
+					</p>
+				</form>
 				<p><a href="<?php echo home_url(); ?>"><?php _e('Vuelve a la p&aacute;gina de inicio', 'ungrynerd'); ?></a></p>
 			</div>
 		</div>
