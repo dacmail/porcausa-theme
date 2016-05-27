b9aafda4e476692a637825fc665de2a6718ed526
Daniel Aguilar <dacmail@gmail.com>
2016-05-27 09:54:39 +0200

    Erorres 404

--- 404.php
+++ 404.php
@@ -1,8 +1,15 @@
 <?php get_header() ?>
-<div id="container" <?php body_class('clearfix'); ?>>
-	<section id="content" class="clearfix">
-		<h1 class="post-title"><?php _e('La p&aacute;gina que buscas no existe', 'ungrynerd'); ?></h1>
-		<p class="post-content"><a href="<?php echo home_url(); ?>"><?php _e('Vuelve a la p&aacute;gina de inicio', 'ungrynerd'); ?></a></p>
-	</section>
+<div class="container">
+	<article class="hentry">
+		<div class="article-wrapper">
+			<h1 class="section-title">
+				<?php _e('La p&aacute;gina que buscas no existe', 'ungrynerd'); ?>
+			</h1>
+
+			<div class="post-content clearfix">
+				<p><a href="<?php echo home_url(); ?>"><?php _e('Vuelve a la p&aacute;gina de inicio', 'ungrynerd'); ?></a></p>
+			</div>
+		</div>
+	</article>
 </div>
 <?php get_footer() ?>
--- single.php
+++ single.php
@@ -1,3 +1,3 @@
 <?php get_header() ?>
-	<?php get_template_part( 'loop', get_post_format() ); ?>
+	<?php get_template_part( 'loop', get_post_format()); ?>
 <?php get_footer() ?>
