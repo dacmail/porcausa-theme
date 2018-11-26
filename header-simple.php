ce13c5eb78488ba2dd003dab82e1f383a0cafb82
Daniel Aguilar <dacmail@gmail.com>
2018-11-26 08:42:39 +0100

    Cambio analytics

--- header-simple.php
+++ header-simple.php
@@ -3,6 +3,12 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-42658230-1');
+  </script>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
